f79228cfe6a3e5991429d12652767055fbeba804
Jerry <JerryWon1214@gmail.com>
2024-04-09 18:17:23 +0800

    新片排行榜十部电影的文字信息

--- method/login.go
+++ method/login.go
@@ -1,18 +1,12 @@
 package method
 
-import (
-	"douban/model"
-	"github.com/go-rod/rod"
-	"time"
-)
-
-func Login(page *rod.Page) {
-	userBreeze := model.NewUser("15680451969", "Wang000616@@..")
-	page.MustNavigate("https://www.douban.com/").MustWaitStable()
-	iframe := page.MustElement(`[frameborder="0"]`).MustFrame()
-	iframe.MustElement(`li.account-tab-account`).MustClick()
-	iframe.MustElement(`#username`).MustInput(userBreeze.Username())
-	iframe.MustElement(`#password`).MustInput(userBreeze.Password())
-	iframe.MustElement(`div.account-form-field-submit `).MustClick()
-	time.Sleep(time.Hour)
-}
+//func Login(page *rod.Page) {
+//	userBreeze := model.NewUser("15680451969", "Wang000616@@..")
+//	page.MustNavigate("https://www.douban.com/").MustWaitStable()
+//	iframe := page.MustElement(`[frameborder="0"]`).MustFrame()
+//	iframe.MustElement(`li.account-tab-account`).MustClick()
+//	iframe.MustElement(`#username`).MustInput(userBreeze.Username())
+//	iframe.MustElement(`#password`).MustInput(userBreeze.Password())
+//	iframe.MustElement(`div.account-form-field-submit `).MustClick()
+//	time.Sleep(time.Hour)
+//}
--- method/utils.go
+++ method/utils.go
@@ -4,48 +4,17 @@ import (
 	"douban/model"
 	"fmt"
 	"github.com/go-rod/rod"
-	"github.com/xuri/excelize/v2"
-	"reflect"
 	"strings"
 	"time"
 )
 
-func writeToExcel(data []model.Movie) {
-	// 创建文件
-	file := excelize.NewFile()
-	sheetName := "Sheet1"
-	// 创建工作表
-	index, _ := file.NewSheet(sheetName)
-
-	// 设置标题行
-	titles := []string{"名称", "上映时间", "评分", "海报"}
-	for i, title := range titles {
-		cell, _ := excelize.CoordinatesToCellName(i+1, 1)
-		file.SetCellValue("Sheet1", cell, title)
-	}
-	// 遍历电影切片，写入每部电影的数据
-	for i, movie := range data {
-		v := reflect.ValueOf(movie)
-		for j := 0; j < v.NumField(); j++ {
-			cell, _ := excelize.CoordinatesToCellName(j+1, i+2) // 行号从2开始，因为第1行已经是列标题了
-			file.SetCellValue(sheetName, cell, v.Field(j).Interface())
-		}
-	}
-	// 设置默认打开的工作表
-	file.SetActiveSheet(index)
-	// 保存文件
-	if err := file.SaveAs("resource/movies.xlsx"); err != nil {
-		println(err.Error())
-	}
-}
-
-// 采集电影详细信息
+// CollectMovieInfo 采集电影详细信息
 func CollectMovieInfo(page *rod.Page) model.Movie {
 	var alias string
 	name := page.MustElement(`span[property="v:itemreviewed"]`).MustText()
 	director := page.MustElement(`a[rel="v:directedBy"]`).MustText()
 	screenWriter := page.MustElement(`#info > span:nth-child(3) > span.attrs > a`).MustText()
-	actors := []string{}
+	var actors []string
 	for _, actor := range page.MustElements(`a[rel="v:starring"]`) {
 		actors = append(actors, strings.TrimSuffix(actor.MustText(), "/"))
 	}
